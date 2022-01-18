5f3121c6da8e844b23586a8d97ae30df4e7d8262
DevExpressExampleBot <fakeEmail>
2022-01-18 12:51:34 +0300

    Source auto update [skip ci]

---
+++ VB/AutoFilterlRow/MainWindow.xaml.vb
@@ -0,0 +1,40 @@
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Grid
+Imports DevExpress.Data.Filtering
+
+Namespace AutoFilterlRow
+
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.grid1.ItemsSource = Data.GetData()
+        End Sub
+    End Class
+
+    Public Class NewTableView
+        Inherits TableView
+
+        Protected Overrides Function CreateAutoFilterCriteria(ByVal fieldName As String, ByVal condition As AutoFilterCondition, ByVal value As Object) As CriteriaOperator
+            If condition = AutoFilterCondition.Equals Then
+                Return MyBase.CreateAutoFilterCriteria(fieldName, condition, value)
+            End If
+
+            Dim strValue As String = value.ToString()
+            If String.IsNullOrEmpty(strValue) Then Return Nothing
+            If condition = AutoFilterCondition.Contains Then
+                Return New FunctionOperator(FunctionOperatorType.Contains, New OperandProperty(fieldName), strValue)
+            Else
+                Return New FunctionOperator(FunctionOperatorType.StartsWith, New OperandProperty(fieldName), strValue)
+            End If
+        End Function
+    End Class
+End Namespace
